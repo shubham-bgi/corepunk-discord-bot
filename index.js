271589327adb3979c49a3a15da464fbd907b6d8d
Shubham <shubhamaggarwal3011@gmail.com>
2023-03-05 23:40:57 +0530

    added rank changes and can work with command

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const rankRairty = require('./data/rank.json');
 const metadata = require("./data/metadata_rank.json");
 const bot = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 const botConfig = require('./botConfig.json');
@@ -14,25 +16,31 @@ bot.on('ready', () => {
 });
 
 bot.on('messageCreate', msg => {
-    if (msg.author.bot || msg.channel.type === 'dm' || !msg.mentions.users.has(bot.user.id)) return;
+    if (msg.author.bot || msg.channel.type === 'dm' || !(msg.mentions.users.has(bot.user.id) || msg.content.startsWith(botConfig.prefix))) return;
     const args = msg.content.split(/ +/);
     const id = +args.pop();
     if (!id || isNaN(id)) {
         msg.channel.send(`Usage: @${bot.user.username} id\nExample: @${bot.user.username} 10`);
         return;
     }
-    if (id < 0 || id > 10000) {
-        msg.channel.send(`Id should be in range of 0 - 10000.`);
+    if (id < 1 || id > 10000) {
+        msg.channel.send(`Id should be in range of 1 - 10000.`);
         return;
     }
 
     console.log(`${msg.author.username}#${msg.author.discriminator} asked for id - ${id}`)
-    const obj = metadata[id];
+    const obj = metadata[id - 1];
+    rankRairty.forEach(ele => {
+        const [lower, upper]  =  ele.rankRange.split('-')
+        if(obj.rank>=lower && obj.rank<=upper) {
+            obj.rarity = ele.rarity;
+        }
+    })
     const embed = new EmbedBuilder()
         .setTitle(obj.name)
-        .setDescription(obj.description)
-        .setImage(obj.image)
-        .setFooter({ text: `Rank - ${obj.rank}` });
+        .setDescription(`**${obj.description}**\n**${obj.rarity} Rank #${obj.rank}**`)
+        .setImage(obj.image);
+        // .setFooter({ text: `**${obj.rarity} Rank #${obj.rank}**` });
 
     msg.channel.send({ embeds: [embed] });
 });
--- rank_script.js
+++ rank_script.js
@@ -1,12 +1,12 @@
 const rarity = require("./data/rarity_weights");
-const metadata = require("./data/_metadata.json");
+const metadata = require("./data/_metadata_copy.json");
 const {writeFileSync} = require('fs');
 let metadataCopy = [...metadata]
 metadataCopy = metadataCopy.map((element, index) => {
     element.score = 0;
     element.attributes.forEach((atribute) => {
         const obj = rarity[atribute.trait_type].find((obj) => obj.trait == atribute.value);
-        element.score += +obj.chance;
+        element.score += +obj.occurrence.split('%').shift();
     });
     return element;
 });
@@ -14,9 +14,8 @@ metadataCopy = metadataCopy.map((element, index) => {
 metadataCopy.sort(function(a, b) {
     let keyA = a.score;
     let keyB = b.score;
-    // Compare the 2 dates
-    if (keyA > keyB) return -1;
-    if (keyA < keyB) return 1;
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
     return 0;
   });
 
@@ -25,7 +24,6 @@ metadataCopy = metadataCopy.map((element, index) => {element.rank = index+1; ret
 metadataCopy.sort(function(a, b) {
 let keyA = a.edition;
 let keyB = b.edition;
-// Compare the 2 dates
 if (keyA < keyB) return -1;
 if (keyA > keyB) return 1;
 return 0;
